2efc7b3e0108966b63c80c71f5c4ce057be72caa
iamashokjat <ashokjakad@gmail.com>
2020-07-04 22:28:50 +0530

    first commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,9 @@
+import React,{Fragment} from 'react'
+
+const About = () =>  
+            <Fragment>
+                <h1>About this App</h1> 
+                <p>App to search Github Users</p> 
+                <p>Version 1.0.0</p>
+            </Fragment>
+export default About;
